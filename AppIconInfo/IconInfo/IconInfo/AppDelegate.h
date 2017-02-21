1299a42f538752aa03ab8b3c1d4b224e7c3ee48c
sybz <wangxin19891226@hotmail.com>
2017-02-21 23:15:52 +0800

    创建git仓库

---
+++ AppIconInfo/IconInfo/IconInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IconInfo
+//
+//  Created by wangsean on 2017/2/21.
+//  Copyright © 2017年 wangsean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppIconInfo/IconInfo/IconInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IconInfo
+//
+//  Created by wangsean on 2017/2/21.
+//  Copyright © 2017年 wangsean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
